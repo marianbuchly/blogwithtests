26f047a7d549596164df37d4626935f66e5ae1ce
marianbuchly <marianbuchly@gmail.com>
2016-05-10 10:41:27 +0200

    posts controller routing and contr and routing spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/show'
+
+  get 'posts/create'
+
+  get 'posts/edit'
+
+  get 'posts/destroy'
+
+  root "posts#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :controller do
+
+  describe "GET #index" do
+    let(:posts) { create_list(:post, 3) }
+
+    it "assigns all posts to @posts" do
+      get :index
+      expect(assigns(:posts)).to eq posts
+    end
+  end
+
+  # describe "GET #show" do
+  #   it "returns http success" do
+  #     get :show
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #create" do
+  #   it "returns http success" do
+  #     get :create
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #edit" do
+  #   it "returns http success" do
+  #     get :edit
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #destroy" do
+  #   it "returns http success" do
+  #     get :destroy
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+end
