8c4b6d443c29378b350299b7733bad423c132947
marianbuchly <marianbuchly@gmail.com>
2016-05-12 15:47:51 +0200

    seeds and likes front page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 print "Seeding Posts"
 
 Post.create ([
-  { title: "My first Blog Post", body: "Very interesting blog", image: "http://mtfeat.site90.com/best1/img/slider1.jpg" },
-  { title: "My second Blog Post", body: "More where that came from" },
-  { title: "My third Blog Post" },
-  { title: "My fourth Blog Post" },
-  { title: "My fifth Blog Post" }
+  { title: "The Basics", body: "Learning Ruby on Rails, Javascript, and working with AJAX and JQuery. All of the basics were covered. Check out soms projects here.", image: "http://mtfeat.site90.com/best1/img/slider1.jpg" },
+  { title: "My second Blog Post", body: "Taking it to the next level with a more indepth look into Rails and React.js. Check out severval projects here." },
+  { title: "My third Blog Post", body: "Trying out some new stuff Ruby on Rails etc" },
+  { title: "My fourth Blog Post", body: "Other nerd things will follow soon" },
+  { title: "My fifth Blog Post", body: "Will look amazing someday" }
   ])
 
   puts " seeded #{Post.count} posts!"
