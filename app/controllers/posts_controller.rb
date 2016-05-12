5d5854f7dca6372e9a10801a6e8383b2b1621b47
marianbuchly <marianbuchly@gmail.com>
2016-05-12 13:25:47 +0200

    added carrierwave styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -56,7 +56,7 @@ class PostsController < ApplicationController
 
  private
  def post_params
-      params.require(:post).permit(:title, :body, :id)
+      params.require(:post).permit(:title, :body, :id, :image)
  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510141035) do
+ActiveRecord::Schema.define(version: 20160512112027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20160510141035) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "image"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
